d28088d100f84fe45c9444936e7bdb89464f21e8
yfumaki <38430689+yfumaki@users.noreply.github.com>
2018-04-19 15:08:41 +0200

    TP_Yann

--- TP01.py
+++ TP01.py
@@ -16,7 +16,63 @@ while a < 130:
 """#Exo4
 ask = input("Insérez un mot : ")
 print(len(ask))"""
-#Exo5
-#valeur = input("Entrez un nombre : ")"""
+""""#Exo5
+a = int(input( " entrez un nombre : "))
+if a%2 == 0:
+    print("pair")
+else:
+    print("impair")
+#"""
+"""#Exo6
+def Exo6():
+    nombre = int(input("Entrez un nombre entre 10 & 20 :"))
+    if (nombre > 20):
+        print("Plus petit.")
+    if (nombre < 10):
+        print("Plus grand.")
+Exo6()"""
 
-#
+#Exo7
+"""def Exo7():
+    nombre = int(input("Insérez un nombre :"))
+    print(nombre + 1)
+    print(nombre + 2)
+    print(nombre + 3)
+    print(nombre + 4)
+    print(nombre + 5)
+    print(nombre + 6)
+    print(nombre + 7)
+    print(nombre + 8)
+    print(nombre + 9)
+    print(nombre + 10)
+
+Exo7()
+"""
+#Exo8"""
+"""def Exo8 ():
+    nombre = int(input("Insèrez un nombre de départ :"))           print(nombre * 1)
+            print(nombre * 2)
+            print(nombre * 3)
+            print(nombre * 4)
+            print(nombre * 5)
+            print(nombre * 6)
+            print(nombre * 7)
+            print(nombre * 8)
+            print(nombre * 9)
+            print(nombre * 10)
+Exo8()"""
+"""#Exo9
+age = int(input("Quel est l'âge de votre enfant?"))
+print(age)"""
+#Exo10
+def Exo10():
+    age = int(input("Quel est l'âge de votre enfant?:"))
+    if(age == 6 or age == 7):
+        print("Poussin")
+    elif(age == 8 or age == 9):
+        print("Pupille")
+    elif (age == 10 or age == 11):
+        print("Minime")
+    elif (age > 12):
+        print("Cadet")
+Exo10()
