d7bdf0b5bd0f61d7d555acb887716052147827f5
yfumaki <38430689+yfumaki@users.noreply.github.com>
2018-04-19 13:57:36 +0200

    Add files via upload

---
+++ TP01.py
@@ -0,0 +1,22 @@
+"""Exo1
+txt = ("Je dois faire des sauvegardes régulières de mes fichiers.")
+print(txt * 500)"""
+
+"""#Ex02
+i = 1
+while i < 1000:
+    print(i)
+    i = i + 2"""
+
+"""#Exo3
+a = 13
+while a < 130:
+    print (a)
+    a = a + 13"""
+"""#Exo4
+ask = input("Insérez un mot : ")
+print(len(ask))"""
+#Exo5
+#valeur = input("Entrez un nombre : ")"""
+
+#
